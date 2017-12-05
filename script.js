8ee507f38fe42db364b984ea4db0a79af5269789
Jake Chavez <jake94chavez@gmail.com>
2017-12-05 12:25:23 -0600

    Round Two

---
+++ script.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+  var button = document.getElementById('button-yellow'); button.addEventListener('click', function (event) { prompt('Join Our Mailing List'); button.innerHTML = 'Thanks for your email!' })
+}
